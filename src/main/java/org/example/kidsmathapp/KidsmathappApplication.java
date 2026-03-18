728e8f5f5cd0444f6684eeac12ce129de6e4252b
DTSmth <ducktapedave85@gmail.com>
2026-03-18 13:26:30 -0400

    init commit

---
+++ src/main/java/org/example/kidsmathapp/KidsmathappApplication.java
@@ -0,0 +1,13 @@
+package org.example.kidsmathapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsmathappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsmathappApplication.class, args);
+    }
+
+}
