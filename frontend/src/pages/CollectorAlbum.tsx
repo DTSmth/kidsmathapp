436c23930767964b6f783956c1e1de32f852966b
DTSmth <ducktapedave85@gmail.com>
2026-03-22 16:42:57 -0400

    remove unused imports

--- frontend/src/pages/CollectorAlbum.tsx
+++ frontend/src/pages/CollectorAlbum.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useChild } from '../context/ChildContext';
 import { getInventory } from '../services/inventory';
